6e5a993005352b0c3777cbbaa9c95a8025ed1e13
CyberAngler93 <mperry37@alaska.edu>
2020-12-04 12:06:49 -0900

    fix: update testing
    Includes a different test and update to format

--- test.py
+++ test.py
@@ -9,64 +9,70 @@ import prims
 def test():
     fail = False
     example_graph_medium = {
-    'A': {'B': 2, 'C': 3},
-    'B': {'A': 2, 'C': 1, 'D': 1, 'E': 4},
-    'C': {'A': 3, 'B': 1, 'F': 5},
-    'D': {'B': 1, 'E': 1},
-    'E': {'B': 4, 'D': 1, 'F': 1},
-    'F': {'C': 5, 'E': 1, 'G': 1},
-    'G': {'F': 1},
+    'A': {'B': 5, 'C': 1},
+    'B': {'A': 5, 'C': 4, 'D': 2, 'E': 3},
+    'C': {'A': 1, 'B': 4, 'F': 1},
+    'D': {'B': 2, 'E': 1, 'G': 2},
+    'E': {'B': 3, 'D': 1, 'F': 2},
+    'F': {'C': 1, 'E': 2},
+    'G': {'D': 2, 'F': 1},
     }
     example_graph_medium_result_A = {
-        'A': {'B'},
-        'B': {'C', 'D'},
-        'D': {'E'},
-        'E': {'F'},
-        'F': {'G'},
+        'A': ['C'],
+        'C': ['F'],
+        'F': ['E'],
+        'E': ['D'],
+        'D': ['B', 'G'],
     }
     example_graph_medium_result_B = {
-        'B': {'D', 'C', 'A'},
-        'D': {'E'},
-        'E': {'F'},
-        'F': {'G'},
+        'B': ['D'],
+        'D': ['E', 'G'],
+        'G': ['F'],
+        'F': ['C'],
+        'C': ['A'],
     }
     example_graph_small = {
         'A': {'B': 2},
         'B': {'A': 2},
     }
     example_graph_small_result_A = {
-        'A': {'B'}
+        'A': ['B']
     }
     example_graph_small_result_B = {
-        'B': {'A'}
+        'B': ['A']
     }
     example_only_root = {
         'A': {'A': '1'},
     }
-
     example_only_root_result = {}
+
     print('Starting Tiny Graph Test Suite')
     print('Testing a Single Vertex')
-    print(f"Expected = {example_only_root_result} \nResult   = {prims.prims(example_only_root, 'A')}\n")
-    if not prims.prims(example_only_root, 'A') == example_only_root_result:
+    print(f"Expected = {example_only_root_result}\nResult   = {prims.prims(example_only_root, 'A')}\n")
+    if prims.prims(example_only_root, 'A') != example_only_root_result:
+        print("failed")
         fail = True
     print("Starting Small Test Suite")
     print('Testing Small Start at node A')
-    print(f"Expected = {example_graph_small_result_A} \nResult   = {prims.prims(example_graph_small, 'A')}")
-    if not prims.prims(example_graph_small, 'A') == example_graph_small_result_A:
+    print(f"Expected = {example_graph_small_result_A}\nResult   = {prims.prims(example_graph_small, 'A')}")
+    if prims.prims(example_graph_small, 'A') != example_graph_small_result_A:
+        print("failed")
         fail = True
     print('Testing Small Start at node B')
-    print(f"Expected = {example_graph_small_result_B} \nResult   = {prims.prims(example_graph_small, 'B')}\n")
-    if not prims.prims(example_graph_small, 'B') == example_graph_small_result_B:
+    print(f"Expected = {example_graph_small_result_B}\nResult   = {prims.prims(example_graph_small, 'B')}\n")
+    if prims.prims(example_graph_small, 'B') != example_graph_small_result_B:
+        print("failed")
         fail = True
     print("Starting Medium Test Suite")
     print('Testing Medium Starting at node A')
-    print(f"Expected = {example_graph_medium_result_A} \nResult   = {prims.prims(example_graph_medium, 'A')}")
-    if not prims.prims(example_graph_medium, 'A') == example_graph_medium_result_A:
+    print(f"Expected = {example_graph_medium_result_A}\nResult   = {prims.prims(example_graph_medium, 'A')}")
+    if prims.prims(example_graph_medium, 'A') != example_graph_medium_result_A:
+        print("failed")
         fail = True
     print('Testing Medium Starting at node B')
-    print(f"Expected = {example_graph_medium_result_B} \nResult   = {prims.prims(example_graph_medium, 'B')}\n")
-    if not prims.prims(example_graph_medium, 'B') == example_graph_medium_result_B:
+    print(f"Expected = {example_graph_medium_result_B}\nResult   = {prims.prims(example_graph_medium, 'B')}\n")
+    if prims.prims(example_graph_medium, 'B') != example_graph_medium_result_B:
+        print("failed")
         fail = True
     if fail:
         print('Tests Completed With Failure')
